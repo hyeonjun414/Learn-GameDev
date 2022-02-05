42d01257604778de470e892080703ca390c04503
hyeonjun414 <wns8363@gmail.com>
2022-02-05 21:10:50 +0900

    study
    Do.
     Lv1. 평균 구하기

---
+++ CPP/CodingTest/Lv1. 평균 구하기/Lv1. 평균 구하기.cpp
@@ -0,0 +1,21 @@
+﻿// Lv1. 평균 구하기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int i = 0; i < arr.size(); i++)
+        answer += arr[i];
+    return answer / arr.size();
+}
+
+int main()
+{
+    vector<int> arr = { 1,56,22,6,3,12,5 };
+    cout << solution(arr) << endl;
+}
