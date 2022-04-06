16ed8097b00fba056774130a8a78e5ffaa71f432
Hyeonjun <wns8363@gmail.com>
2022-04-06 20:24:55 +0900

    델리게이트와 이벤트

---
+++ CSharp/LearnCSharp/Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Delegate
+{
+    // 델리게이트는 반환형과 매개변수가 같아야한다.
+    // 이벤트와 마찬가지로 리스트 형태로 저장해 동시에 호출할 수 있다.
+    public class Test
+    {
+        public delegate void TestDelegate(int a);
+        public TestDelegate testDelegate;
+
+        public void TestFunc1(int a)
+        {
+            Console.WriteLine("Func1 호출됨. 들어온 숫자는 " + a.ToString());
+        }
+        public void TestFunc2(int a)
+        {
+            Console.WriteLine("Func2 호출됨. 들어온 숫자에 6을 곱하면 " + (a*6).ToString());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.testDelegate += test.TestFunc1;
+            test.testDelegate += test.TestFunc2;
+            test.testDelegate += test.TestFunc2;
+            test.testDelegate -= test.TestFunc2;
+
+            // 함수포인터에 아무 함수가 배치되어있지않으면 오류가 발생한다.
+            // Invoke는 변수를 함수 형태로 사용하여 델리게이트라고 알 수 있도록 한다.
+            test.testDelegate?.Invoke(10);
+
+            
+        }
+    }
+}
---
+++ CSharp/LearnCSharp/Event/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Event
+{
+    #region 센더 리시버 기본
+    public class Sender
+    {
+        public event EventHandler Event;
+        public delegate void StrDelegate(string a);
+        public StrDelegate strDelegate;
+        public void DoEvent()
+        {
+            Event?.Invoke(this, EventArgs.Empty);
+        }
+        public void DoText(string name)
+        {
+            strDelegate?.Invoke(name);
+        }
+    }
+    public class Receiver
+    {
+        public void CanWriteLine(object sender, EventArgs e)
+        {
+            Console.WriteLine("나는 출력할 수 있다.");
+        }
+        public void StringCount(string str)
+        {
+            Console.WriteLine("이 문장은 " + str.Length + "글자 이다.");
+        }
+        public void senderExcute(Sender sender)
+        {
+            sender.strDelegate.Invoke("나나");
+        }
+    }
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sender sender = new Sender();
+            Receiver receiver = new Receiver();
+
+            sender.Event += receiver.CanWriteLine;
+            sender.DoEvent();
+
+            sender.strDelegate = receiver.StringCount;
+
+            sender.strDelegate.Invoke("다섯글자임");
+
+            receiver.senderExcute(sender);
+        }
+    }
+}
