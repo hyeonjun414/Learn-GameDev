9e991c9dbca6322b4cffb10a86cbab4fccb42587
Hyeonjun <wns8363@gmail.com>
2022-04-10 21:53:29 +0900

    유니티 에이스타
    클릭하여 노드 색상변경까지

---
+++ Unity/활용 스크립트/Astar/Scripts/Board.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeColor
+{
+    Normal,
+    Denial,
+    Open,
+    Close,
+    Start,
+    End,
+
+    Size
+}
+
+
+
+public class Board : MonoBehaviour
+{
+    public static int[,] map =
+    {
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1}
+     };
+    public int x, y;
+    [SerializeField]
+    public ASNode[,] ASNodes;
+
+    public NodeColor color;
+
+    public Material[] nodeColors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ASNodes = new ASNode[x, y];
+        ASNode[] nodes = gameObject.GetComponentsInChildren<ASNode>();
+
+        int n = 0;
+        foreach (ASNode node in nodes)
+        {
+            //node.rend.material = nodeColors[(int)NodeColor.Normal];
+            ASNodes[(int)(n/x), (int)(n%x)] = node;
+            ASNodes[(int)(n / x), (int)(n % x)].rend.material = nodeColors[0];
+            ASNodes[(int)(n / x), (int)(n % x)].point = new Point((int)(n / x), (int)(n % x));
+            n++;
+            print(((int)(n / x)).ToString() +", "+ ((int)(n % x)).ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputClick();
+    }
+    void InputClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject go = hit.transform.gameObject;
+                print(go);
+                ASNode node = go.GetComponent<ASNode>();
+                if(null != node)
+                {
+                    node.rend.material = nodeColors[(int)color];
+                }
+            }
+        }
+    }
+}
---
+++ Unity/활용 스크립트/Astar/Scripts/MainUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    public Board board;
+    public Dropdown nodeTypeDropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i<(int)NodeColor.Size; i++)
+        {
+            NodeColor nc = (NodeColor)i;
+            Dropdown.OptionData data = new Dropdown.OptionData();
+            data.text = nc.ToString();
+            nodeTypeDropdown.options.Add(data);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnNodeTypeChanged()
+    {
+        int value = nodeTypeDropdown.value;
+        if(0 != value)
+        {
+            board.color = (NodeColor)(nodeTypeDropdown.value - 1);
+        }
+    }
+}
