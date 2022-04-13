fb8294458cf5cb8a73e73fd7e0b9e2e2adbd6c08
Hyeonjun <wns8363@gmail.com>
2022-04-13 21:52:11 +0900

    포톤 통신 테스트

---
+++ Unity/Photon_Test/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviour
+{
+    public PhotonView pv;
+    public SpriteRenderer sr;
+    void Update()
+    {
+        if(pv.IsMine)
+        {
+            MoveFunc();
+        }
+    }
+    void MoveFunc()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, y, 0);
+        transform.Translate(move*10*Time.deltaTime);
+
+        if (x != 0) pv.RPC("FlipXRPC", RpcTarget.AllBuffered, x);
+
+    }
+
+    [PunRPC]
+    void FlipXRPC(float axis)
+    {
+        // 왼쪽이면 True, 아니면 false
+        sr.flipX = axis == -1;
+    }
+}
