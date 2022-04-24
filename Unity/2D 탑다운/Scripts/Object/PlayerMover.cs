a054a016d1004a50e423c49bbdcfc1eb414c3ea7
Hyeonjun <wns8363@gmail.com>
2022-04-24 19:50:03 +0900

    2D 탑다운 실습 3
    아이템 - 인벤토리 시스템

--- Unity/2D 탑다운/Scripts/Object/PlayerMover.cs
+++ Unity/2D 탑다운/Scripts/Object/PlayerMover.cs
@@ -23,11 +23,18 @@ public class PlayerMover : MonoBehaviour, IAttackable
     public Projectile Arrow;
     public Transform shotPoint;
 
+    public Transform weaponPoint;
+    public Weapon weapon;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         coll = GetComponent<Collider2D>();
+    }
+    private void Start()
+    {
+        EquipManager.Instance.player = this;
     }
     private void Update()
     {
@@ -109,36 +116,58 @@ public class PlayerMover : MonoBehaviour, IAttackable
     {
         if (isActing || isAttack) return;
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetKeyDown(KeyCode.X) && weapon != null)
+        {
+            Vector2 dirVec = Vector2.zero;
+            float dir = anim.GetFloat("PrevDir");
+
+            isAttack = true;
+            anim.SetTrigger("IsMelee");
+            anim.SetBool("IsAttack", isAttack);
+            weapon.AttackStart();
+
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
         {
             Vector2 dirVec = Vector2.zero;
             float dir = anim.GetFloat("PrevDir");
             float rotAngle = 0;
             switch (dir)
             {
-                case 0: dirVec = Vector2.up; rotAngle = 0;      break;
-                case 1: dirVec = Vector2.down; rotAngle = 180;  break;
-                case 2: dirVec = Vector2.left; rotAngle = 90;  break;
-                case 3: dirVec = Vector2.right; rotAngle = -90;  break;
+                case 0: dirVec = Vector2.up; rotAngle = 0; break;
+                case 1: dirVec = Vector2.down; rotAngle = 180; break;
+                case 2: dirVec = Vector2.left; rotAngle = 90; break;
+                case 3: dirVec = Vector2.right; rotAngle = -90; break;
             }
 
-            Projectile arrow = Instantiate<Projectile>(Arrow, shotPoint.position, Quaternion.identity * Quaternion.Euler(0,0, rotAngle));
+            Projectile arrow = Instantiate(Arrow, shotPoint.position, Quaternion.identity * Quaternion.Euler(0, 0, rotAngle));
             arrow.SetUp(arrowDamage, 2);
             arrow.rb.AddForce(dirVec * ShotPower);
+            
+            anim.SetTrigger("IsRange");
             isAttack = true;
             anim.SetBool("IsAttack", isAttack);
-            
+
         }
     }
+    public void AttackStart()
+    {
+        
+    }
+
     public void AttackEnd()
     {
         isAttack = false;
         anim.SetBool("IsAttack", isAttack);
+
+        if(weapon != null)
+        {
+            weapon.AttackEnd();
+        }
     }
 
     void DropItem()
     {
-
         if (Input.GetKeyDown(KeyCode.Z) && InventoryManager.Instance.items.Count > 0)
         {
             Vector2 dirVec = Vector2.zero;
@@ -154,5 +183,35 @@ public class PlayerMover : MonoBehaviour, IAttackable
             Instantiate(obj, transform.position + (Vector3)dirVec, Quaternion.identity);
             InventoryManager.Instance.items[0].Remove();
         }
+    }
+
+    public bool Equip(EquipInvenItem item)
+    {
+        if(item.eqiupType == EquipmentType.Weapon)
+        {
+            GameObject newWeapon = Instantiate(item.equipPrefab, weaponPoint.position, Quaternion.identity, weaponPoint);
+            weapon = newWeapon.GetComponent<Weapon>();
+            weapon.gameObject.SetActive(false);
+            return true;
+        }
+        else if(item.eqiupType == EquipmentType.Armor)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    public bool UnEquip(EquipmentType type)
+    {
+        switch(type)
+        {
+            case EquipmentType.Weapon:
+                Destroy(weapon.gameObject);
+                weapon = null;
+                return true;
+            case EquipmentType.Armor: 
+                return true;
+        }
+        return false;
     }
 }
