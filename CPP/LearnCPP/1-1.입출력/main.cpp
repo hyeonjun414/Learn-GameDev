3f7718d2a6a344de46b4206d7852b6a7bcd401e1
Hyeonjun <wns8363@gmail.com>
2022-05-03 21:41:21 +0900

    유니티 3D 디펜스게임 - 1
    마우스 클릭 기반 스크립팅

--- CPP/LearnCPP/1-1.입출력/main.cpp
+++ CPP/LearnCPP/1-1.입출력/main.cpp
@@ -5,12 +5,37 @@ using namespace std;
 //입출력
 int main()
 {
-	int n;
+	//int n;
 
-	cout << "Hello World!" << endl << "숫자를 입력하세요 : ";
-	cin >> n;
+	//cout << "Hello World!" << endl << "숫자를 입력하세요 : ";
+	//cin >> n;
 
-	cout << "입력 결과 : " << n << endl;
+	//cout << "입력 결과 : " << n << endl;
 
-	return 0;
+	//return 0;
+	int* p = new int[5]{ 1,2,4,8,16 };
+	int value1 = p[4] - p[1];
+	int value2 = (p + 4) - (p + 1);
+	int value3 = (long)(p + 4) - (long)(p + 1);
+
+	cout << value1 + value2 + value3 << endl;
+	cout << (p+4) - (p+2) << endl;
+	cout << value1 << endl;
+	cout << value2 << endl;
+	cout << value3 << endl;
+
+	int total = 0;
+
+	for (size_t i = 1; i <= 10; i++)
+	{
+		if (i % 2) continue;
+		for (size_t j = 1; j <= 10; j++)
+		{
+			for (size_t k = 0; k <=10; k++)
+			{
+				total += i == j && j == k ? i : 0;
+			}
+		}
+	}
+	cout << total;
 }
